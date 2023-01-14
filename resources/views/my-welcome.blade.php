a881ddaf2b7fd83ad025becdaea4fb27be3e3bcd
Ali Nisham <alinisham@live.com>
2023-01-14 20:23:27 +0500

    Laraboot_FirstTry

---
+++ resources/views/my-welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>
+                        Students Data
+                        <div class="btn btn-primary btn-sm float-end">
+                            Add Student
+                        </div>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
