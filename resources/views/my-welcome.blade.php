d408d205071a975589cd9b8e62e61769c8b20796
Maldeefi <alinisham@live.com>
2023-01-30 01:19:21 +0500

    Updated list

--- resources/views/my-welcome.blade.php
+++ resources/views/my-welcome.blade.php
@@ -6,21 +6,53 @@
             <div class="card">
                 <div class="card-header">
                     <h4>
-                        Students Data
+                        Profiles
                         <div class="btn btn-primary btn-sm float-end">
                             Add Student
                         </div>
                     </h4>
                 </div>
                 <div class="card-body">
-                    <table class="table">
-                        <thead>
+                    <table class="table table-striped">
+                        <thead class="thead-dark">
                             <th scope="col">#</th>
-                            <th scope="col">#</th>
-                            <th scope="col">#</th>
-                            <th scope="col">#</th>
-                            <th scope="col">#</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">Delete / Edit</th>
                         </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Student</td>
+                                <td>Ahmed</td>
+                                <td>Ahmed</td>
+                                <td>
+                                    <div class="btn btn-primary btn-sm">Edit</div>
+                                    <div class="btn btn-danger btn-sm">Delete</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Student</td>
+                                <td>Ahmed</td>
+                                <td>Ahmed</td>
+                                <td>
+                                    <div class="btn btn-primary btn-sm">Edit</div>
+                                    <div class="btn btn-danger btn-sm">Delete</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Student</td>
+                                <td>Ahmed</td>
+                                <td>Ahmed</td>
+                                <td>
+                                    <div class="btn btn-primary btn-sm">Edit</div>
+                                    <div class="btn btn-danger btn-sm">Delete</div>
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
